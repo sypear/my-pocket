3d0aa14be03a3e5ab3e5a694131e9eb313b153fe
seungyeon.bae <bsyeon015@gmail.com>
2022-08-03 16:53:56 +0900

    :sparkles: 내역 추가 div 클릭 여부에 따라 내역 추가 폼 open/close하는 기능 추가

--- src/components/NewAsset/NewAsset.js
+++ src/components/NewAsset/NewAsset.js
@@ -5,13 +5,31 @@ import NewAssetForm from "./NewAssetForm/NewAssetForm";
 import "./NewAsset.css";
 
 const NewAsset = () => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+
     return (
-        <div className="new-asset">
-            {/* <span className="fs-normal fw-bold">내역 추가하기</span> */}
-            <div className="new-asset__create">
-                <h1 className="fs-normal">내역 추가</h1>
-                <NewAssetForm />
-            </div>
+        <div className="new-asset" style={{textAlign: !isEditing ? 'center' : 'left', cursor: !isEditing ? 'pointer' : 'auto'}}>
+            {
+                !isEditing &&
+                <div className="fs-normal fw-bold new-asset__create-btn" onClick={startEditingHandler}>
+                    내역 추가하기
+                </div>
+            }
+            {
+                isEditing &&
+                <div className="new-asset__create">
+                    <h1 className="fs-normal">내역 추가</h1>
+                    <NewAssetForm onCancle={stopEditingHandler} />
+                </div>
+            }
         </div>
     );
 };
