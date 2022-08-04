b1ccd03554a16700b3cc521582ee82131a6586b4
seungyeon.bae <bsyeon015@gmail.com>
2022-08-04 10:33:39 +0900

    :sparkles: Asset Data Handler 추가 및 Asset 컴포넌트로 assets 전달

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewAsset from "./components/NewAsset/NewAsset";
 import Asset from "./components/Asset/Asset";
@@ -7,12 +7,23 @@ import "./components/UI/Font.css";
 import "./components/UI/Button.css";
 
 const App = () => {
-  return (
-    <>
-      <NewAsset />
-      <Asset />
-    </>
-  );
+	const [latestAssetId, setLatestAssetId] = useState(0);
+	const [assets, setAssets] = useState("");
+
+	const addAssetDataHandler = (assetData) => {
+		setLatestAssetId(latestAssetId + 1);
+		
+		setAssets(prevAssets => {
+			return [assetData, ...prevAssets];
+		});
+	}
+
+	return (
+		<>
+			<NewAsset onAddAssetData={addAssetDataHandler} latestAssetId={latestAssetId} />
+			<Asset assets={assets} />
+		</>
+	);
 };
 
 export default App;
