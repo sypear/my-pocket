9a08d5f704acd198a8e9769c8d31f77fd3a76b58
seungyeon.bae <bsyeon015@gmail.com>
2022-08-04 19:09:06 +0900

    :art: AssetStatus 컴포넌트를 AssetList 아래로 이동

--- src/components/Asset/AssetList/AssetStatus/AssetStatus.js
+++ src/components/Asset/AssetList/AssetStatus/AssetStatus.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { addComma } from "../../../common/number";
+import { addComma } from "../../../../common/number";
 
 import "./AssetStatus.css";
 
@@ -8,12 +8,13 @@ const AssetStatus = (props) => {
     const [totalAssets, setTotalAssets] = useState(0);
     const [totalIncome, setTotalIncome] = useState(0);
     const [totalExpense, setTotalExpense] = useState(0);
+    const year = props.filteredYear.slice(-2);
 
     useEffect(() => {
+        let total = {assets: 0, income: 0, expense: 0};
+
         if (props.assets.length > 0) {
             // 자산, 수입, 지출 합계 계산
-            let total = {assets: 0, income: 0, expense: 0};
-    
             props.assets.forEach(item => {
                 if (item.amount_type === "income") {
                     total.assets += +item.amount;
@@ -23,17 +24,17 @@ const AssetStatus = (props) => {
                     total.expense += +item.amount;
                 }
             });
-
-            setTotalAssets(total.assets);
-            setTotalIncome(total.income);
-            setTotalExpense(total.expense);
         }
+
+        setTotalAssets(total.assets);
+        setTotalIncome(total.income);
+        setTotalExpense(total.expense);
     }, [props.assets]);
 
     return (
         <>
             <div className="title">
-                <h1 className="fs-normal fw-light">22년 자산 현황</h1>
+                <h1 className="fs-normal fw-light">{year}년 자산 현황</h1>
                 <strong className="fs-title">{addComma(totalAssets.toString())}원</strong>
             </div>
 
