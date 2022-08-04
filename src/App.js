aa667d763a23f396c34659f8f17ca5c721fd7254
seungyeon.bae <bsyeon015@gmail.com>
2022-08-04 20:13:44 +0900

    :adhesive_bandage: 로컬스토리지 저장 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import NewAsset from "./components/NewAsset/NewAsset";
 import Asset from "./components/Asset/Asset";
@@ -8,7 +8,31 @@ import "./components/UI/Button.css";
 
 const App = () => {
 	const [latestAssetId, setLatestAssetId] = useState(0);
-	const [assets, setAssets] = useState("");
+	const [assets, setAssets] = useState([]);
+
+	useEffect(() => {
+		const localAssets = JSON.parse(localStorage.getItem("assets"));
+		const localLatestAssetId = +localStorage.getItem("latestAssetId");
+
+		if (localAssets === null) {
+			localStorage.setItem("assets", JSON.stringify(assets));
+		localStorage.setItem("latestAssetId", latestAssetId);
+		} else {
+			let copyLocalAssets = [...localAssets];
+
+			copyLocalAssets.forEach((element, i) => {
+				copyLocalAssets[i].date = new Date(element.date)
+			});
+
+			setAssets(copyLocalAssets);
+			setLatestAssetId(localLatestAssetId);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("assets", JSON.stringify(assets));
+		localStorage.setItem("latestAssetId", latestAssetId);
+	}, [assets]);
 
 	const addAssetDataHandler = (assetData) => {
 		setLatestAssetId(latestAssetId + 1);
