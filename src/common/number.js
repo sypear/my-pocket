f86e76d385f385e40966e443d5c14e4691a3a089
seungyeon.bae <bsyeon015@gmail.com>
2022-08-04 10:29:05 +0900

    :art: 숫자 관련 함수들을 공통 모듈로 분리

---
+++ src/common/number.js
@@ -0,0 +1,11 @@
+export const enteredOnlyNumber = (val) => {
+    return val.replace(/[^0-9]/g,'');
+}
+
+export const addComma = (num) => {
+    return num.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+export const deleteComma = (str) => {
+    return str.replace(/,/g, "");
+}
