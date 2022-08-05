cefbbc35e66f45935c29f854826934416be8e309
seungyeon.bae <bsyeon015@gmail.com>
2022-08-05 19:51:35 +0900

    :recycle: 년도를 두글자로 표시하는 변수의 이름을 좀 더 명확하게 수정 (year → twoDigitYear)

--- src/components/Pocket/PocketStatus.js
+++ src/components/Pocket/PocketStatus.js
@@ -6,7 +6,7 @@ const PocketStatus = (props) => {
     const [totalAssets, setTotalAssets] = useState(0);
     const [totalIncome, setTotalIncome] = useState(0);
     const [totalExpense, setTotalExpense] = useState(0);
-    const year = props.filteredYear.slice(-2);
+    const twoDigitYear = props.filteredYear.slice(-2);
 
     useEffect(() => {
         let total = {assets: 0, income: 0, expense: 0};
@@ -32,6 +32,7 @@ const PocketStatus = (props) => {
     return (
         <div className="pocket__status">
             <div className="pocket__status-title">
+                <h1 className="fs-normal fw-light">{twoDigitYear}년 자산 현황</h1>
                 <strong className="fs-title">{addComma(totalAssets.toString())}원</strong>
             </div>
 
